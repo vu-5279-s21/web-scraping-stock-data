ed55aa5f502be5534f47a5937421328d0985823d
Christopher Rorex <christopherrorex1@gmail.com>
2021-04-11 21:53:18 -0500

    Updated ticker page and added home page functionality. 4/11 10 pm

--- WebScraping/main/backend/views.py
+++ WebScraping/main/backend/views.py
@@ -81,4 +81,25 @@ def results(request):
     return render(request, 'main/results.html')
 
 def tickers(request):
-    return render(request, 'main/ticker.html')
+    result1 = ''
+    page = requests.get('https://stockanalysis.com/stocks/')
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    table = soup.find(class_='no-spacing')
+
+    tickers_companies = table.find_all("a")
+
+    my_data =[tick.get_text() for tick in tickers_companies]
+
+    my_tickers = [data.partition('-')[0].strip() for data in my_data]
+    my_companies = [data.partition('-')[2].strip() for data in my_data]
+
+    ticker_table = pd.DataFrame({
+        'ticker_symbol': my_tickers,
+        'company_name': my_companies
+    })
+    if 'ticker' in request.GET:
+        ticker = request.GET.get('ticker')
+        result1 = ticker_table[ticker_table['company_name'] == ticker]['ticker_symbol']
+
+    return render(request, 'main/ticker.html', {'result': result1})
